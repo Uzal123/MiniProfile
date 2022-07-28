69e16d58548e9c62f4b43aebe844f2e4453ef37f
Ujjwal-1214 <ujwalpaswan1214@gmail.com>
2022-07-29 01:47:03 +0530

    resume

--- pages/index.js
+++ pages/index.js
@@ -75,6 +75,32 @@ export default function Home() {
         </a>
         <div className="flex justify-center gap-1 p-2">
           <a
+            href="https://ujjwal-resume.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-gray-300 w-10 h-10 rounded-md flex items-center justify-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+              />
+            </svg>
+          </a>
+          <a
             href="https://wa.me/+918592031812"
             target="_blank"
             rel="noopener noreferrer"
