d0534f507cc86709dcfbb67172bb31587784ce87
Ujjwal-1214 <ujwalpaswan1214@gmail.com>
2022-09-03 20:14:45 +0530

    img link update

--- pages/index.js
+++ pages/index.js
@@ -164,7 +164,7 @@ export default function Home() {
               <ProjectItems
                 title="Hotel App"
                 url="https://dazziieltd.netlify.app/"
-                imglink={"./dashboard.PNG"}
+                imglink={"./dashboard.png"}
               />
               <ProjectItems
                 title="Travel Company"
